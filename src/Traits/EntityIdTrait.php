c04a3d7f969be87daf4a2df1eee84ff1d3a192fd
Yan Rogozinsky <janwebdev@gmail.com>
2023-06-02 22:42:06 +0300

    updating EntityIdTrait.php

--- src/Traits/EntityIdTrait.php
+++ src/Traits/EntityIdTrait.php
@@ -6,7 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 trait EntityIdTrait
 {
     #[ORM\Id]
-    #[ORM\GeneratedValue(strategy: 'AUTO')]
+    #[ORM\GeneratedValue(strategy: 'SEQUENCE')]
     #[ORM\Column(type: 'integer')]
 	private int $id;
 
